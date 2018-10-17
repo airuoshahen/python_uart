dd2143997b1b3c1242b6eb48526fde06dbc5dfc9
airuoshahen <1044878025@qq.com>
2018-10-17 22:58:53 +0800

    gui and com

--- uart.py
+++ uart.py
@@ -1,33 +1,41 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[18]:
+# In[14]:
 
 
-import serial
-import serial.tools.list_ports
+from tkinter import *
 
-#下面5--18行的程序可以打开一个串口
-plist = list(serial.tools.list_ports.comports())
-
-if len(plist) <= 0:
-    print('The Serial port can not find!')
-else:
-    plist_0 = list(plist[0])
-    serialName = plist_0[0]
-    print(serialName)
-    serialFd = serial.Serial(serialName,115200,timeout=0.5)
-    print('check which port was really used >',serialFd.name)
-    if serialFd.isOpen():
-        print(serialFd.name,' is open!')
+def btnClick():
+    plist = list(serial.tools.list_ports.comports())
+    
+    if len(plist) <= 0:
+        print('The serial port can not find!')
+        return
     else:
-        print(serialFd.name,' is close!')
-#下面19--25行的程序是如何运用串口读取发送数据
-n = serialFd.write('uart test!'.encode())
-print(serialFd.portstr)
-print(n)
-readBytes = serialFd.read(n)
-readStr = bytes.decode(readBytes)
-print(readStr)
-serialFd.close()
+        plist_0 = list(plist[0])
+        serialName = plist_0[0]
+        serialFd = serial.Serial(serialName,115200,timeout=0.5)
+        if serialFd.isOpen():
+            print(serialFd.name,' is open!')
+        else:
+            print(serialFd.name,' is close!')
+        serialFd.write('RESET\n'.encode())
+        readBytes = serialFd.readline()
+        readStr = bytes.decode(readBytes)
+        print(readStr)
+        serialFd.close()
+        return
+
+root = Tk(className="我的第一个窗口程序");
+
+textLabel = Label(root,text = '提示显示',justify=LEFT,padx=100)
+textLabel.pack(side = TOP)
+
+btn = Button(root)
+btn['text']='RESET'
+btn['command'] = btnClick
+btn.pack(side = BOTTOM)
+
+mainloop()
 
